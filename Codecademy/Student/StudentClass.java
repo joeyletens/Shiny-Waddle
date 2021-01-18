5c04477f9955917466c6f8df80ab11c2a1f8a3a4
JoeyLetens <jletens076@gmail.com>
2021-01-18 13:32:55 +0100

    Completed Student Class for real (TEST GIT)

--- Codecademy/Student/StudentClass.java
+++ Codecademy/Student/StudentClass.java
@@ -1,6 +1,6 @@
 package Student;
 
-public class StudentClass() {
+public class StudentClass {
   String email;
   String firstName;
   String lastName;
@@ -11,6 +11,19 @@ public class StudentClass() {
   String postalCode;
   String city;
   String country;
-  
-  
+
+  public StudentClass(String email, String firstName, String lastName, String dateOfBirth, String gender, String street,
+      String houseNumber, String postalCode, String city, String country) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.gender = gender;
+    this.street = street;
+    this.houseNumber = houseNumber;
+    this.postalCode = postalCode;
+    this.city = city;
+    this.country = country;
+  }
+
 }
