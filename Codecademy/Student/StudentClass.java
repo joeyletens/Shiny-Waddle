52d471b9229b71ccc87207539d06f0a284122c89
JoeyLetens <jletens076@gmail.com>
2021-01-18 12:59:50 +0100

    (Still testing) Added StudentClass variables

--- Codecademy/Student/StudentClass.java
+++ Codecademy/Student/StudentClass.java
@@ -1,6 +1,16 @@
 package Student;
 
 public class StudentClass() {
+  String email;
   String firstName;
   String lastName;
+  String dateOfBirth;
+  String gender;
+  String street;
+  String houseNumber;
+  String postalCode;
+  String city;
+  String country;
+  
+  
 }
