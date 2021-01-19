0a3002ff8cd291cd9a64b49a60474d5255f88649
JoeyLetens <jletens076@gmail.com>
2021-01-19 15:19:21 +0100

    Fixed names, added class variables and constructor to certificate and registration

---
+++ Codecademy/Certificate/Class/Certificate.java
@@ -0,0 +1,14 @@
+package Certificate.Class;
+
+public class Certificate {
+  int certificateID;
+  double grade;
+  String employeeName;
+
+  public Certificate(int certificateID, double grade, String employeeName) {
+    this.certificateID = certificateID;
+    this.grade = grade;
+    this.employeeName = employeeName;
+  }
+
+}
