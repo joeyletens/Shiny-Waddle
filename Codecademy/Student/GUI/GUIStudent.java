cc70493387995e7dbcb8917b7d790531c16be403
JoeyLetens <jletens076@gmail.com>
2021-01-19 10:21:00 +0100

    Changed update to change, added menu button functionality

--- Codecademy/Student/GUI/GUIStudent.java
+++ Codecademy/Student/GUI/GUIStudent.java
@@ -1,15 +1,11 @@
 package Student.GUI;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
 
 public class GUIStudent extends Application {
 
@@ -28,101 +24,125 @@ public class GUIStudent extends Application {
     Label studentLabel = new Label("Student");
     Button createStudentButton = new Button("Add");
     Button readStudentButton = new Button("Read");
-    Button updateStudentButton = new Button("Update");
+    Button changeStudentButton = new Button("Change");
     Button deleteStudentButton = new Button("Delete");
+    Label emailLabel = new Label("Email:");
 
     // 1.3 Add buttons to menu
-    menu.getChildren().addAll(studentLabel, createStudentButton, readStudentButton, updateStudentButton, deleteStudentButton);
+    menu.getChildren().addAll(studentLabel, createStudentButton, readStudentButton, changeStudentButton,
+        deleteStudentButton);
     mainLayout.setTop(menu);
 
     // 2. Create CREATE STUDENT subview layout
     VBox createStudentView = new VBox(new Label("Enter new student's details below"));
     createStudentView.setSpacing(30);
+    Button addStudent = new Button("add student");
 
     // 3. Create READ STUDENT subview layout
     VBox readStudentView = new VBox(new Label("Enter student's email which you would like to read more about"));
     readStudentView.setSpacing(30);
 
-    // 4. Create UPDATE STUDENT subview layout
-    VBox updateStudentView = new VBox(new Label("Enter student's email which you would like to update"));
-    updateStudentView.setSpacing(30);
+    // 4. Create CHANGE STUDENT subview layout
+    VBox changeStudentView = new VBox(new Label("Enter student's email which you would like to change"));
+    changeStudentView.setSpacing(30);
 
     // 5. Create READ STUDENT subview layout
     VBox deleteStudentView = new VBox(new Label("Enter student's email which you would like to delete"));
     deleteStudentView.setSpacing(30);
 
-    // Add horizontal FIRSTNAME box
+    // 2. Add horizontal FIRSTNAME box
     TextField inputFirstName = new TextField("");
     HBox firstName = new HBox(new Label("First name:"));
     firstName.getChildren().add(inputFirstName);
 
-    // Add horizontal LASTNAME box
+    // 2. Add horizontal LASTNAME box
     TextField inputLastName = new TextField("");
     HBox lastName = new HBox(new Label("Last name:"));
     lastName.getChildren().add(inputLastName);
 
-    // Add horizontal EMAIL box
+    // 2. Add horizontal EMAIL box
     TextField emailInput = new TextField("");
-    HBox email = new HBox(new Label("Email:"));
+    HBox email = new HBox(emailLabel);
     email.getChildren().add(emailInput);
 
-    // Add horizontal DATEOFBIRTH box
+    // 2. Add horizontal DATEOFBIRTH box
     TextField dateOfBirthInput = new TextField("");
     HBox dateOfBirth = new HBox(new Label("Date of birth:"));
     dateOfBirth.getChildren().add(dateOfBirthInput);
 
-    // Add horizontal GENDER box
+    // 2. Add horizontal GENDER box
     TextField genderInput = new TextField("");
     HBox gender = new HBox(new Label("Gender:"));
     gender.getChildren().add(genderInput);
 
-    // Add horizontal POSTALCODE box
+    // 2. Add horizontal POSTALCODE box
     TextField postalCodeInput = new TextField("");
     HBox postalCode = new HBox(new Label("Enter your postalcode:"));
     postalCode.getChildren().add(postalCodeInput);
 
-    // Add horizontal STREET box
+    // 2. Add horizontal STREET box
     TextField inputStreet = new TextField("");
     HBox street = new HBox(new Label("Enter your street:"));
     street.getChildren().add(inputStreet);
 
-    // Add horizontal HOUSENUMBER box
+    // 2. Add horizontal HOUSENUMBER box
     TextField inputHouseNumber = new TextField("");
     HBox houseNumber = new HBox(new Label("Enter your house number:"));
     houseNumber.getChildren().add(inputHouseNumber);
 
-    // Add horizontal CITY box
+    // 2. Add horizontal CITY box
     TextField inputCity = new TextField("");
     HBox city = new HBox(new Label("Enter your city:"));
     city.getChildren().add(inputCity);
 
-    // Add horizontal COUNTRY box
+    // 2. Add horizontal COUNTRY box
     TextField inputCountry = new TextField("");
     HBox country = new HBox(new Label("Enter your country:"));
     country.getChildren().add(inputCountry);
 
-    // 4 add label to UPDATE STUDENT 
-    Label updateText = new Label("Fill in the textfields which you would like to update")
+    // 4 add label to CHANGE STUDENT
+    Label changeText = new Label("Fill in the textfields which you would like to change");
+    // Add horizontal EMAIL box
+    TextField emailInputChange = new TextField("");
+    HBox emailChange = new HBox(emailLabel);
+    emailChange.getChildren().add(emailInputChange);
+
+    // 3. Add horizontal READ EMAIL box
+    TextField emailInputRead = new TextField("");
+    HBox emailRead = new HBox(emailLabel);
+    emailRead.getChildren().add(emailInputRead);
+
+    // 5. Add horizontal READ EMAIL box
+    TextField emailInputDelete = new TextField("");
+    HBox emailDelete = new HBox(emailLabel);
+    emailDelete.getChildren().add(emailInputDelete);
 
     // 2.1 add to parent node ADD TO CREATE STUDENT FORM
     createStudentView.getChildren().addAll(firstName, lastName, email, dateOfBirth, gender, postalCode, street,
-        houseNumber, city, country);
+        houseNumber, city, country, addStudent);
 
-    // 3.1 add to parent node ADD TO READ STUDENT FORM
-    readStudentView.getChildren().addAll(email);
+    // // 3.1 add to parent node ADD TO READ STUDENT FORM
+    // readStudentView.getChildren().add(emailRead);
 
-    // 4.1 add to parent node ADD TO UPDATE STUDENT FORM
-    updateStudentView.getChildren().addAll(email, updateText, firstName, lastName, email, dateOfBirth, gender,
-        postalCode, street, houseNumber, city, country);
+    // // // 4.1 add to parent node ADD TO CHANGE STUDENT FORM
+    // // changeStudentView.getChildren().addAll(emailChange, changeText, firstName,
+    // // lastName, email, dateOfBirth, gender,
+    // // postalCode, street, houseNumber, city, country);
 
-    // 5.1 add to parent node ADD TO DELETE STUDENT FORM
-    deleteStudentView.getChildren().addAll(email);
+    // // 5.1 add to parent node ADD TO DELETE STUDENT FORM
+    // deleteStudentView.getChildren().add(emailDelete);
 
-    // 6. set scene to create student AS STANDARD
+    // 6.1 add subview buttons
+    createStudentButton.setOnAction((event) -> mainLayout.setCenter(createStudentView));
+    readStudentButton.setOnAction((event) -> mainLayout.setCenter(readStudentView));
+    changeStudentButton.setOnAction((event) -> mainLayout.setCenter(changeStudentView));
+    deleteStudentButton.setOnAction((event) -> mainLayout.setCenter(deleteStudentView));
+
+    // 6.2 set scene to create student AS STANDARD
     mainLayout.setCenter(createStudentView);
     Scene view = new Scene(mainLayout);
 
-    // Start application, set title, set scene and show window
+    // 7. Start application, set title, set scene and show window
     window.setTitle("Joey Letens");
     window.setScene(view); // Set the scene
     window.show();
